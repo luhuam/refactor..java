402de302937d4e6308eb23c44faf1f6f924750d8
Lucero Marysol <67833726+luhuam@users.noreply.github.com>
2023-07-13 15:26:49 -0500

    Update Refactor_java.java

--- src/main/java/refactor/refactor_java/Refactor_java.java
+++ src/main/java/refactor/refactor_java/Refactor_java.java
@@ -4,24 +4,24 @@
  */
 
 package refactor.refactor_java;
-import java.util.logging.Logger;
-import java.util.Arrays;
 
-/**
- *
- * @author DELL
- */
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class Refactor_java {
 
-    public static void main(String[] args) {
+  private static final Logger logger = Logger.getLogger(Refactor_java.class.getName());
+
+  public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
     String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
 
     if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+      String errorMessage = "Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result;
+      logger.severe(errorMessage);
+      throw new AssertionError(errorMessage);
     }
     logger.info("Success");
-
   }
 }
