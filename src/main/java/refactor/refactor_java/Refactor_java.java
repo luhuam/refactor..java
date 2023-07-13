b405c21477175a00c6878ab5684da93afdf437c6
Lucero <lucero.marysol.huaman.ampuero@gmail.com>
2023-07-13 13:49:03 -0500

    commmit

---
+++ src/main/java/refactor/refactor_java/Refactor_java.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package refactor.refactor_java;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author DELL
+ */
+public class Refactor_java {
+
+    public static void main(String[] args) {
+    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+
+    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+
+    if (!result.equals(expected)) {
+      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+    }
+
+    System.out.println("Success");
+  }
+}
